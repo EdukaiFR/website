ff6e3f27a0451bdea60d8b2820959cf5b60866ef
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-04 20:22:35 +0800

    feat: init nextjs & shadcnui projet with outfit font and color theme

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="outfit-bold text-5xl text-primary-500">
+        Bienvenue sur Edukai!
+      </h1>
+    </div>
+  );
+}
