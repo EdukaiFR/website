e4af368c4f18028a2f6a52a4ecb4c7f6bfc7f5bf
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-08 09:40:53 +0800

    fix: routing when user are logged in or not

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const loginLinks = {
-  Accueil: "/",
+  Accueil: "/home",
   Générateur: "/generator",
   Club_Edukai: "/clubEdukai",
   Mes_Cours: "/myCourses",
-  Mon_Profil: "/myProfile",
 };
 
 const guestLinks = {
@@ -20,55 +20,69 @@ const guestLinks = {
 };
 
 export const Header = () => {
+  const router = useRouter();
   const [isLogin, setIsLogin] = useState<boolean>(false);
   const [linksToShow, setLinksToShow] =
     useState<Record<string, string>>(guestLinks);
-
-  // Initialize selectedLink with an empty string
   const [selectedLink, setSelectedLink] = useState<string>("");
 
-  // Use useEffect to safely access window object after the component mounts
+  // Charger l'état de connexion depuis localStorage au montage du composant
   useEffect(() => {
-    setSelectedLink(window.location.pathname);
+    const storedLoginStatus = localStorage.getItem("isLogin");
+    if (storedLoginStatus) {
+      const parsedLoginStatus = JSON.parse(storedLoginStatus);
+      setIsLogin(parsedLoginStatus);
+      setLinksToShow(parsedLoginStatus ? loginLinks : guestLinks);
+      setSelectedLink(
+        parsedLoginStatus ? loginLinks.Accueil : guestLinks.Solution
+      );
+    }
   }, []);
 
-  useEffect(() => {
-    if (isLogin) {
-      setLinksToShow(loginLinks);
-    } else {
-      setLinksToShow(guestLinks);
+  // Mettre à jour localStorage et les liens affichés seulement quand isLogin change
+  const handleLoginToggle = () => {
+    const newLoginStatus = !isLogin;
+    setIsLogin(newLoginStatus);
+    localStorage.setItem("isLogin", JSON.stringify(newLoginStatus));
+    setLinksToShow(newLoginStatus ? loginLinks : guestLinks);
+    setSelectedLink(newLoginStatus ? loginLinks.Accueil : guestLinks.Solution);
+    if (newLoginStatus) {
+      router.push(loginLinks.Accueil);
+    } else if (!newLoginStatus) {
+      router.push(guestLinks.Solution);
     }
-  }, [isLogin]);
+  };
 
   return (
     <div className="relative flex items-center justify-between w-full py-[2%] px-4">
-      {/* Left-aligned title */}
       <h2 className="text-xl text-primary-500 outfit-bold">Edukai</h2>
-
-      {/* Center-aligned menu */}
       <div className="absolute left-1/2 transform -translate-x-1/2">
         <div className="flex items-center gap-5">
           {Object.entries(linksToShow).map(([name, url]) => (
             <a
               key={name}
               href={url}
-              onClick={() => setSelectedLink(url)}
-              className={`relative transition-all text-md text-white hover:text-primary-500 outfit-regular
-                ${
-                  selectedLink === url
-                    ? "after:content-[''] after:absolute after:left-0 after:right-0 after:bottom-[-4px] after:w-[75%] after:h-[2px] after:bg-primary-500 after:rounded-full"
-                    : ""
-                } `}
+              onClick={(e) => {
+                e.preventDefault(); // Empêcher le rechargement
+                setSelectedLink(url);
+                router.push(url);
+              }}
+              className={`relative transition-all text-md text-white hover:text-primary-500 outfit-regular ${
+                selectedLink === url
+                  ? "after:content-[''] after:absolute after:left-0 after:right-0 after:bottom-[-4px] after:w-[75%] after:h-[2px] after:bg-primary-500 after:rounded-full"
+                  : ""
+              }`}
             >
               {name.replace("_", " ")}
             </a>
           ))}
         </div>
       </div>
-
-      {/* Right-aligned button */}
-      <Button className="rounded-full outfit-regular text-sm text-white px-[2.5%] py-[1%] ml-auto">
-        Se connecter
+      <Button
+        className="rounded-full outfit-regular text-sm text-white px-[2.5%] py-[1%] ml-auto"
+        onClick={handleLoginToggle}
+      >
+        {isLogin ? "Se déconnecter" : "Se connecter"}
       </Button>
     </div>
   );
