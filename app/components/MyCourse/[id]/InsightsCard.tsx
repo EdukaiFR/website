7bb7e7a4b9cbb4d81f738e38520a08f65884ae74
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-16 12:08:11 +0800

    feat: create two comp for insightCard with primary or accent theme

---
+++ app/components/MyCourse/[id]/InsightsCard.tsx
@@ -0,0 +1,46 @@
+export type InsightsCardProps = {
+  title: string;
+  value: number;
+  unit: string;
+  base: string;
+};
+
+export const InsightsCardAccent = ({
+  title,
+  value,
+  unit,
+  base,
+}: InsightsCardProps) => {
+  return (
+    <div className="p-5 bg-accent bg-opacity-25 text-accent outfit-regular rounded-lg border-2 border-accent flex flex-col items-start gap-3 w-full">
+      <p className="text-md text-opacity-50">{title.toUpperCase()}</p>
+      <div className="flex items-center justify-center gap-1 w-full">
+        <p className="text-5xl outfit-regular">
+          {value}
+          {unit}
+        </p>
+        <p className="text-sm text-accent text-opacity-75">{base}</p>
+      </div>
+    </div>
+  );
+};
+
+export const InsightsCardPrimary = ({
+  title,
+  value,
+  unit,
+  base,
+}: InsightsCardProps) => {
+  return (
+    <div className="p-5 bg-primary bg-opacity-25 text-primary outfit-regular rounded-lg border-2 border-primary flex flex-col items-start gap-3 w-full">
+      <p className="text-md text-opacity-50">{title.toUpperCase()}</p>
+      <div className="flex items-center justify-center gap-1 w-full">
+        <p className="text-5xl outfit-regular">
+          {value}
+          {unit}
+        </p>
+        <p className="text-sm text-primary text-opacity-75">{base}</p>
+      </div>
+    </div>
+  );
+};
