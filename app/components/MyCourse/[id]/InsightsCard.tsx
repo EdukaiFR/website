fb3b4f52fa865ad29d1dfaf7bcca07fda30aede0
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-16 13:12:32 +0800

    fix: opacity-75 on title value

--- app/components/MyCourse/[id]/InsightsCard.tsx
+++ app/components/MyCourse/[id]/InsightsCard.tsx
@@ -13,7 +13,9 @@ export const InsightsCardAccent = ({
 }: InsightsCardProps) => {
   return (
     <div className="p-5 bg-accent bg-opacity-25 text-accent outfit-regular rounded-lg border-2 border-accent flex flex-col items-start gap-3 w-full">
-      <p className="text-md text-opacity-50">{title.toUpperCase()}</p>
+      <p className="text-md text-accent text-opacity-75">
+        {title.toUpperCase()}
+      </p>
       <div className="flex items-center justify-center gap-1 w-full">
         <p className="text-5xl outfit-regular">
           {value}
@@ -33,7 +35,9 @@ export const InsightsCardPrimary = ({
 }: InsightsCardProps) => {
   return (
     <div className="p-5 bg-primary bg-opacity-25 text-primary outfit-regular rounded-lg border-2 border-primary flex flex-col items-start gap-3 w-full">
-      <p className="text-md text-opacity-50">{title.toUpperCase()}</p>
+      <p className="text-md text-primary text-opacity-75">
+        {title.toUpperCase()}
+      </p>
       <div className="flex items-center justify-center gap-1 w-full">
         <p className="text-5xl outfit-regular">
           {value}
