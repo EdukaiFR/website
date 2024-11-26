6f966d66de48506d0236345bdf2b7550c06728a6
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-11-26 23:39:06 +0100

    refactor: move test data to a separate folder
    This cleans up the code by removing the json testing data from the components/page files. Also removed quiz-related data from the course JSON, as we get it separately.

--- app/hooks/quizGenerator.ts
+++ app/hooks/quizGenerator.ts
@@ -1,41 +1,11 @@
 import { QuizService } from "@/services";
 import { useState } from "react";
+import testData from "../json/testData/quizResponse.json";
 
-// Temp value of quizData to remove when QuizService is implemented
-export const tempData = {
-  success: true,
-  message: [
-    {
-      question:
-        "Quelle est la caractéristique des propositions indépendantes selon le contenu donné ?",
-      choices: [
-        "A. Elles sont reliées par une ponctuation forte",
-        "B. Elles sont autonomes et ont un seul verbe conjugué",
-        "C. Elles ont plusieurs verbes conjugués",
-        "D. Elles dépendent d'une autre proposition",
-      ],
-      answer: "B. Elles sont autonomes et ont un seul verbe conjugué",
-      explanation:
-        "Les propositions indépendantes sont autonomes et contiennent un seul verbe conjugué.",
-    },
-    {
-      question:
-        "Comment sont souvent séparées les propositions indépendantes selon le contenu donné ?",
-      choices: [
-        "A. Par des conjonctions de coordination",
-        "B. Par des conjonctions de subordination",
-        "C. Par une ponctuation forte",
-        "D. Par une ponctuation faible",
-      ],
-      answer: "D. Par une ponctuation faible",
-      explanation:
-        "Les propositions indépendantes sont souvent séparées par une ponctuation faible comme une virgule ou un point-virgule.",
-    },
-  ],
-};
+export const tempData = testData; // Temporary test data for the quiz
 
 export function useQuizGenerator(quizService: QuizService) {
-  const [quizData, setQuizData] = useState(null);
+  const [quizData, setQuizData] = useState({});
   const [isGenerating, setIsGenerating] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
