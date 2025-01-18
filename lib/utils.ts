5d72b45cf2c1212370984966fea0f2ae4d99f35f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-01-18 10:30:41 +0800

    refactor: do rebase with dev branche & do merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getDaysLeft(date: Date) {
+  const today = new Date();
+  const timeDiff = date.getTime() - today.getTime();
+  return Math.ceil(timeDiff / (1000 * 3600 * 24));
 }
