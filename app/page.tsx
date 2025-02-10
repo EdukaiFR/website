ad7a9cf75198aa5c3f7d0cdab2ace854d0e731ca
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-10 12:53:56 +0800

    feat: create skeleton for the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  // TODO: Create a useNavigation hook to handle useRouter and other navigation-related logic
-  // const router = useRouter();
-  // const { getProfile } = useProfile();
-  // const [profile, setProfile] = useState<any>(null);
-  // const { user, loading } = useUser();
-  // const [userId, setUserId] = useState<string | null>(null);
+  return (
+    <div className="flex flex-col gap-6 px-6 min-h-[calc(100vh-5rem)]">
+      {/* Titre principal */}
+      <div className="flex flex-col gap-1 items-start">
+        <h1 className="text-4xl font-bold text-[#3C517C]">Bienvenue Tristan</h1>
+        <p className="text-md text-medium-muted">
+          Voici un petit résumé de ton compte
+        </p>
+      </div>
 
-  // const fetchProfile = async (userId: string) => {
-  //   try {
-  //     const profile = await getProfile(userId);
-  //     setProfile(profile);
-  //   } catch (error: any) {
-  //     console.error("Error fetching profile:", error.message);
-  //   }
-  // };
+      {/* Grid Layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 flex-1">
+        {/* Bloc 1 : Prochain examen */}
+        <div className="col-span-1 bg-gray-200 p-6 rounded-lg flex items-center justify-center">
+          <p className="text-xl font-bold text-gray-600">Examen à venir</p>
+        </div>
 
-  // useEffect(() => {
-  //   if (!loading && user?.id && !userId) {
-  //     fetchProfile(user.id);
-  //     setUserId(user.id);
-  //   }
-  // }, [user, loading]);
+        {/* Bloc 2 : Tes objectifs */}
+        <div className="col-span-1 md:col-span-1 lg:col-span-2 bg-gray-300 p-6 rounded-lg flex items-center justify-center">
+          <p className="text-xl font-bold text-gray-600">Objectifs</p>
+        </div>
 
-  // if (loading || !user || !profile) {
-  //   return <p>Loading user information...</p>;
-  // }
-
-  return <div className="flex flex-1 flex-col gap-4 p-4 pt-0">OUIIIII</div>;
+        {/* Bloc 3 : Activité récente */}
+        <div className="col-span-1 md:col-span-2 lg:col-span-3 bg-gray-400 p-6 rounded-lg flex flex-1 items-center justify-center flex-grow w-full">
+          <p className="text-xl font-bold text-gray-600">Activité récente</p>
+        </div>
+      </div>
+    </div>
+  );
 }
