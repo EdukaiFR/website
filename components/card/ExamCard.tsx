236e58254bd831727366bbabda14d1ddb3c617e8
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-14 14:04:35 +0800

    re

--- components/card/ExamCard.tsx
+++ components/card/ExamCard.tsx
@@ -1,3 +1,5 @@
+import { getDaysLeft, formatDate } from "@/lib/date-format";
+
 export type ExamCardProps = {
   exam: {
     examId: number;
@@ -8,33 +10,6 @@ export type ExamCardProps = {
 };
 
 export const ExamCard = ({ exam }: ExamCardProps) => {
-  function getDaysLeft(date: Date | string) {
-    const today = new Date();
-    const targetDate = new Date(date);
-    const timeDiff = targetDate.getTime() - today.getTime();
-    return Math.ceil(timeDiff / (1000 * 3600 * 24));
-  }
-
-  function formatDate(dateString: string): string {
-    const months = [
-      "janvier",
-      "février",
-      "mars",
-      "avril",
-      "mai",
-      "juin",
-      "juillet",
-      "août",
-      "septembre",
-      "octobre",
-      "novembre",
-      "décembre",
-    ];
-
-    const [day, month, year] = dateString.split("/").map(Number);
-    return `${day} ${months[month - 1]} ${year}`;
-  }
-
   return (
     <div className="rounded-lg bg-white border border-[#E3E3E7] flex flex-col gap-6 items-start justify-between w-full p-4">
       {/* Header */}
