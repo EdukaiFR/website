800cc9d3eda15d0369649622cf46b43f76f4c2dc
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-14 14:03:33 +0800

    refactor: move all 'temp' data in this folder

---
+++ public/mocks/default-value.tsx
@@ -0,0 +1,65 @@
+// TempValue for exam
+export const examsValue = [
+  {
+    examId: 1,
+    title: "Contrôle",
+    description: "Réviser tout le premier chapitre",
+    date: new Date(2025, 1, 25), // 25 février 2025 (février = 1)
+  },
+  {
+    examId: 2,
+    title: "Bac blanc",
+    description: "Réviser tous les chapitres",
+    date: new Date(2025, 2, 27), // 27 mars 2025 (mars = 2)
+  },
+];
+
+// TempValue for resumeFiles
+export const resumeFilesValue = [
+  {
+    id: 1,
+    src: "/temp/Fiche-RevisionFrancaisPro.png",
+    alt: "Fiche de révision",
+    added_at: "2025-02-02T05:42:41.971Z",
+    origin: "Edukai",
+  },
+  {
+    id: 2,
+    src: "/temp/Fiche-RevisionFrancaisPro.png",
+    alt: "Fiche de révision 2",
+    added_at: "2025-02-03T05:42:41.971Z",
+    origin: "Edukai",
+  },
+  {
+    id: 3,
+    src: "/temp/Fiche-RevisionFrancaisPro.png",
+    alt: "Fiche de révision 3",
+    added_at: "2025-02-10T05:42:41.971Z",
+    origin: "Toi",
+  },
+];
+
+// TempValue for rankins
+export const rankings = [
+  {
+    user: {
+      firstname: "Tristan",
+      lastname: "H",
+    },
+    score: 1236,
+  },
+  {
+    user: {
+      firstname: "Khalid",
+      lastname: "B",
+    },
+    score: 1135,
+  },
+  {
+    user: {
+      firstname: "Lucas",
+      lastname: "R",
+    },
+    score: 1024,
+  },
+];
