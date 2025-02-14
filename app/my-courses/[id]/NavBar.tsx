83777ff2c6688187b903f04064a9ed0a1d06706e
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-14 12:16:57 +0800

    refactor: save selected tab in URL

--- app/my-courses/[id]/NavBar.tsx
+++ app/my-courses/[id]/NavBar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export type NavBarProps = {
   tabs: { label: string; tab: string }[];
@@ -6,17 +10,36 @@ export type NavBarProps = {
   selectedTab: string;
 };
 
-export const NavBar = ({ setSelectedTab, tabs, selectedTab }: NavBarProps) => {
+export const NavBar = ({ tabs, setSelectedTab, selectedTab }: NavBarProps) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // Met à jour l'état et l'URL lorsqu'un onglet est sélectionné
+  const handleTabChange = (tab: string) => {
+    setSelectedTab(tab);
+    const newUrl = `${pathname}?tab=${tab}`;
+    router.push(newUrl, { scroll: false });
+  };
+
+  // Synchronisation avec l'URL au chargement ou lorsque l'URL change
+  useEffect(() => {
+    const tabFromUrl = searchParams.get("tab");
+    if (tabFromUrl && tabFromUrl !== selectedTab) {
+      setSelectedTab(tabFromUrl);
+    }
+  }, [searchParams, setSelectedTab]);
+
   return (
     <div className="w-full flex items-center justify-start gap-3 outfit-regular text-medium-white">
       {tabs.map((tab) => (
         <Button
           key={tab.tab}
-          onClick={() => setSelectedTab(tab.tab)}
+          onClick={() => handleTabChange(tab.tab)}
           className={`transition-all ${
             selectedTab === tab.tab
               ? "bg-[#3678FF] text-white hover:bg-[#3678FF]/80"
-              : "bg-[#E3E3E7] bg-opacity-15 text-[#1A202C] text-opacity-75 hover:bg-[#1A202C]/10 text-medium-black  "
+              : "bg-[#E3E3E7] bg-opacity-15 text-[#1A202C] text-opacity-75 hover:bg-[#1A202C]/10 text-medium-black"
           } px-5 rounded-full text-sm`}
         >
           {tab.label}
