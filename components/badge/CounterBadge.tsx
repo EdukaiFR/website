47d3d13f02d303c57e9d09cd332a899572203288
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-14 14:03:15 +0800

    refactor: add two optional parameter -> size & type

--- components/badge/CounterBadge.tsx
+++ components/badge/CounterBadge.tsx
@@ -1,11 +1,28 @@
 export type CounterBadgeProps = {
   counter: number;
+  type?: string;
+  size?: "xs" | "sm" | "md" | "lg";
 };
 
-export const CounterBadge = ({ counter }: CounterBadgeProps) => {
+export const CounterBadge = ({
+  counter,
+  type,
+  size = "sm",
+}: CounterBadgeProps) => {
+  // Définition des classes de taille
+  const sizeClasses = {
+    xs: "text-xs px-2 py-0.5",
+    sm: "text-sm px-3 py-1",
+    md: "text-base px-4 py-1.5",
+    lg: "text-lg px-5 py-2",
+  };
+
   return (
-    <div className="rounded-full flex items-center justify-center bg-[#2D6BCF] bg-opacity-25 text-[#2D6BCF] satoshi-medium text-xs px-4 py-1">
-      {counter}
+    <div
+      className={`rounded-full flex items-center justify-center bg-[#2D6BCF] bg-opacity-25 text-[#2D6BCF] satoshi-medium gap-1 ${sizeClasses[size]}`}
+    >
+      <p>{counter}</p>
+      {type && <p>{type}</p>}
     </div>
   );
 };
