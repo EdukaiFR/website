967baca4945f829e121e49f43ac95784f00070d9
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-02-17 10:22:56 +0800

    refactor: rename my-courses folder to library for consistency

--- app/library/[id]/sections/resume-files/ResumeFiles.tsx
+++ app/library/[id]/sections/resume-files/ResumeFiles.tsx
@@ -1,4 +1,4 @@
-import { columns } from "@/app/my-courses/[id]/sections/resume-files/columns";
+import { columns } from "@/app/library/[id]/sections/resume-files/columns";
 import { CounterBadge } from "@/components/badge/CounterBadge";
 import { DataTable } from "@/components/data-table";
 import { resumeFilesValue } from "@/public/mocks/default-value";
