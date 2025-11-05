bd828c5608cd86148f60e2fa09fb898bdea5fb50
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2025-11-05 09:41:43 +0100

    refactor(auth): create specific file for auth components

---
+++ config/auth/features.config.ts
@@ -0,0 +1,14 @@
+import { CheckCircle2 } from "lucide-react";
+import { LucideIcon } from "lucide-react";
+
+export interface FeatureConfig {
+    text: string;
+    icon: LucideIcon;
+}
+
+export const features: FeatureConfig[] = [
+    { text: "Génération en 20 secondes", icon: CheckCircle2 },
+    { text: "Questions personnalisées", icon: CheckCircle2 },
+    { text: "Suivi de progression", icon: CheckCircle2 },
+    { text: "100% gratuit", icon: CheckCircle2 },
+];
